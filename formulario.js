a8754445b4eecf98fbc8ad89fa292c4178124fe4
abrahamLillo26 <abr.lillo@duocuc.cl>
2021-05-10 23:30:23 -0400

    arreglo al form

--- formulario.js
+++ formulario.js
@@ -15,16 +15,12 @@ $(document).ready(function () {
             },
             apellido: {
                 required: true,
-                minlength: 3,
-                maxlength: 20
+                minlength: 4,
+                maxlength: 25
             },
             email: {
                 required: true,
                 email: true
-            },
-            comentario: {
-                required: true,
-                minlength: 20
             },
             celular: {
                 required: true,
@@ -34,7 +30,7 @@ $(document).ready(function () {
             },
             mensaje: {
                 required: true,
-                minlength: 3,
+                minlength: 20,
                 maxlength: 200
             },
 
@@ -42,28 +38,28 @@ $(document).ready(function () {
         messages: {
             nombre: {
                 required: "Debes ingresar el nombre",
-                minlength: "El nombre debe tener mas de 3 letras",
-                maxlength: "El nombre exece la cantidad de letras o caracteres (40)"
+                minlength: "El nombre debe tener más de 3 caracteres",
+                maxlength: "El nombre excede la cantidad de caracteres permitidos (25)"
             },
             apellido: {
                 required: "Debes ingresar el apellido",
-                minlength: "El apellido debe tener mas de 3 letras",
-                maxlength: "El apellido exece la cantidad de letras o caracteres (40)"
+                minlength: "El apellido debe tener más de 3 caracteres",
+                maxlength: "El apellido excede la cantidad de caracteres permitidos (25)"
             },
             email: {
                 required: "Debes ingresar un email",
                 email: "Debes ingresar un email válido ej: 'abcd@gmail.com'"
             },
             celular: {
-                required: "debes ingresar un celular",
+                required: "Debes ingresar un celular",
                 number: "El celular debe contener 8 números",
-                minlength: "El celular ingresado debe contener 8 numeros",
-                maxlength: "El celular ingresado debe contener 8 numeros"
+                minlength: "El celular ingresado debe contener 8 números",
+                maxlength: "El celular ingresado debe contener 8 números"
             },
             mensaje: {
                 required: "Debes ingresar un mensaje",
-                minlength: "El mensaje debe tener como minimo de 20 letras o caracteres",
-                maxlength: "El mensaje debe tener mas de 20 letras o caracteres y como maximo 200 caracteres o letras"
+                minlength: "El mensaje debe tener como mínimo 20 caracteres",
+                maxlength: "El mensaje debe tener como máximo 200 caracteres"
             },
             
 
